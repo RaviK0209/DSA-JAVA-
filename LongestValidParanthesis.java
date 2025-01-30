ed9b89c14a93af589a40f50a5c8a84e3809eda36
RaviK0209 <ravikant18712@gmail.com>
2025-01-30 21:13:44 +0530

    hard leetcode

---
+++ LongestValidParanthesis.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+
+class LongestValidParanthesis {
+    public int longestValidParentheses(String s) {
+      Stack<Integer> stack = new Stack<>();
+      stack.push(-1);
+      int max=0;
+      for(int i=0;i<s.length();i++){
+        Character ch = s.charAt(i);
+        if(ch == '('){
+            stack.push(i);
+        }
+        else{
+            stack.pop();
+            if(stack.isEmpty()){
+                stack.push(i);
+            }
+            max = Math.max(max,i-stack.peek());
+        }
+      }
+    return max;
+    }
+}
